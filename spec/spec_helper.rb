9304030d132c7e6eac2a3d255ab054c9acaed5ac
Ricardo Valeriano <mister.sourcerer@gmail.com>
2025-07-18 15:48:41 -0300

    Make it possible to use helper methods on console.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,25 @@
 
 require "taxator"
 
+module Taxator
+  module SpecHelper
+    def _fixtures_path
+      @path ||= File.expand_path("../fixtures", __FILE__)
+    end
+
+    def fixture_path(name)
+      path = File.join _fixtures_path, name
+      return nil if !File.exist?(path)
+      path
+    end
+
+    def fixture(name)
+      path = fixture_path(name)
+      File.read(path) if path
+    end
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -12,4 +31,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include Taxator::SpecHelper
 end
